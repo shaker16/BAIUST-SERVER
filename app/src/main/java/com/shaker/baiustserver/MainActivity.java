80df0f5e110778f549d3af131845623afc09c5f7
shaker16 <shakeralshawon16@gmail.com>
2022-05-13 22:48:52 +0600

    BAIUST SERVER

---
+++ app/src/main/java/com/shaker/baiustserver/MainActivity.java
@@ -0,0 +1,24 @@
+package com.shaker.baiustserver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView mWebView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.mWebView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://baiustserver.com");
+
+
+    }
+}
